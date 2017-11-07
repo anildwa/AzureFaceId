01f7496236033cac5bfffc266311602d04608471
Anil Dwarakanath <anildwa@microsoft.com>
2017-11-07 16:39:15 +0530

    tset

---
+++ AzureFaceId/Controllers/FaceApiController.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.IO;
+using System.Threading;
+using Microsoft.ProjectOxford.Face;
+using Microsoft.WindowsAzure.Storage.Table;
+
+
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AzureFaceId
+{
+    [Route("api/[controller]")]
+    public class FaceApiController : Controller
+    {
+
+        const string _subscriptionKey = "";
+        static FaceServiceClient faceServiceClient = new FaceServiceClient(_subscriptionKey, "https://southeastasia.api.cognitive.microsoft.com/face/v1.0");
+        const string personGroupId = "anildwaid1";
+        const string personGroupName = "anildwa1";
+
+        // GET: api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public async Task<string> Post(HttpRequestMessage req)
+        {
+            var response = "";
+
+            CloudTable PersonInfo = await UploadPersonDataController.CreateTableAsync("PersonInfo");
+
+            using (Stream s = Request.Body)
+            {
+                var faces = await faceServiceClient.DetectAsync(s);
+                var faceIds = faces.Select(face => face.FaceId).ToArray();
+
+                var results = await faceServiceClient.IdentifyAsync(personGroupId, faceIds);
+                foreach (var identifyResult in results)
+                {
+                    Console.WriteLine("Result of face: {0}", identifyResult.FaceId);
+                    if (identifyResult.Candidates.Length == 0)
+                    {
+                        Console.WriteLine("No one identified");
+                    }
+                    else
+                    {
+                        // Get top 1 among all candidates returned
+                        var candidateId = identifyResult.Candidates[0].PersonId;
+                        var person = await faceServiceClient.GetPersonAsync(personGroupId, candidateId);
+
+                        var personInfo = await GetTableEntities<PersonInfo>(PersonInfo, person.PersonId.ToString());
+
+                        response = $"{personInfo.First().PersonName},{personInfo.First().PersonCompany}"; 
+                        //Console.WriteLine("Identified as {0}", person.Name);
+                        
+                       
+                    }
+                }
+            }
+
+
+            return response;
+
+            //Stream datastream = await req.Content.ReadAsStreamAsync();
+
+            //datastream.Position = 0;
+           
+
+        }
+
+
+        public static async Task<List<T>> GetTableEntities<T>(CloudTable table, string partitionKey) where T : ITableEntity, new()
+        {
+            Console.WriteLine($"Fetching {typeof(T)}....");
+
+            TableQuery<T> partitionScanQuery =
+            new TableQuery<T>().Where(TableQuery.GenerateFilterCondition("PartitionKey", QueryComparisons.Equal, partitionKey));
+
+            TableContinuationToken token = null;
+
+
+            long tickCount = Environment.TickCount;
+            TableQuerySegment<T> segment;
+            List<T> tableItems = new List<T>();
+
+
+            do
+            {
+                segment = await table.ExecuteQuerySegmentedAsync<T>(partitionScanQuery, token);
+                tableItems.AddRange(segment.ToList());
+                token = segment.ContinuationToken;
+
+            } while (token != null);
+
+            long currentTick = Environment.TickCount;
+            double elapsedTime = Math.Round((currentTick - tickCount) / 1000.0, 3);
+            Console.WriteLine($"Fetch Completed for {typeof(T)}.");
+            Console.Write("\rTotal Time Elapsed={0} secs.  Table Row Count:{1} ", Math.Round(elapsedTime, 3), tableItems.Count());
+
+            return tableItems;
+
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
+
---
+++ AzureFaceId/Controllers/UploadPersonDataController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.IO;
+using System.Threading;
+using Microsoft.ProjectOxford.Face;
+using Microsoft.WindowsAzure.Storage.Table;
+using Microsoft.WindowsAzure.Storage;
+
+
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AzureFaceId
+{
+    [Route("api/[controller]")]
+    public class UploadPersonDataController : Controller
+    {
+
+        const string _subscriptionKey = "";
+        static FaceServiceClient faceServiceClient = new FaceServiceClient(_subscriptionKey, "https://southeastasia.api.cognitive.microsoft.com/face/v1.0");
+        const string personGroupId = "anildwaid1";
+        const string personGroupName = "anildwa1";
+
+        // GET: api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public async Task<string> Post(HttpRequestMessage req)
+        {
+
+            Console.WriteLine("Uploading Image...");
+            try
+            {
+
+                string response = "";
+
+                var createPersonResult = await faceServiceClient.CreatePersonAsync(Request.Query["personGroupId"], Request.Query["personName"]);
+
+                response = $"Person Created: {createPersonResult.PersonId.ToString()}\n";
+
+
+                Console.WriteLine($"Person Created: {createPersonResult.PersonId.ToString()}\n");
+
+
+
+                using (Stream s = Request.Body)
+                {
+                    var addPersonFaceResult = await faceServiceClient.AddPersonFaceAsync(Request.Query["personGroupId"], createPersonResult.PersonId, s);
+                    response = response + $"Face Added: {addPersonFaceResult.PersistedFaceId.ToString()}\n";
+
+                    Console.WriteLine($"Face Added: {addPersonFaceResult.PersistedFaceId.ToString()}\n");
+
+                    CloudTable PersonInfo = await CreateTableAsync("PersonInfo");
+                    TableBatchOperation batchOperation = new TableBatchOperation();
+                    var personEntity = new PersonInfo()
+                    { PersonName = Request.Query["personName"], PersonCompany = Request.Query["personCompany"] };
+
+                    var tableEntity = personEntity as TableEntity;
+                    tableEntity.PartitionKey = createPersonResult.PersonId.ToString();
+                    tableEntity.RowKey = addPersonFaceResult.PersistedFaceId.ToString();
+
+
+                    batchOperation.Insert(personEntity);
+                    IList<TableResult> results = await PersonInfo.ExecuteBatchAsync(batchOperation);
+                    if (results.Count > 0)
+                    {
+                        response = response + $"Table Updated\n";
+                        Console.WriteLine($"Table Updated\n");
+
+                    }
+
+                    await faceServiceClient.TrainPersonGroupAsync(personGroupId);
+                    Console.WriteLine($"Training Started\n");
+
+                }
+
+
+                return response;
+            }
+            catch (Exception ex)
+            {
+
+                return ex.ToString();
+            }
+            
+
+        }
+
+        public static async Task<CloudTable> CreateTableAsync(string tableName)
+        {
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse("");
+            CloudTableClient tableClient = storageAccount.CreateCloudTableClient();
+
+            CloudTable table = tableClient.GetTableReference(tableName);
+            try
+            {
+                if (await table.CreateIfNotExistsAsync())
+                {
+                    //Console.WriteLine("Created Table named: {0}", tableName);
+                }
+                else
+                {
+                    //Console.WriteLine("Table {0} already exists", tableName);
+                }
+            }
+            catch (StorageException ex)
+            {
+                Console.WriteLine(ex.ToString());
+                Console.ReadLine();
+                throw;
+            }
+            //Console.WriteLine();
+            return table;
+        }
+
+
+
+
+
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+
+
+
+    public class PersonInfo : TableEntity
+    {
+
+        public string PersonName { get; set; }
+        public string PersonCompany { get; set; }
+
+
+        
+
+
+    }
+}
